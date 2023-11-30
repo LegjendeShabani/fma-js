fcb5f0e00f0306e559a935f74aacaf68c7d8ddeb
Legjende Shabani <legjendeshabani@gmail.com>
2023-11-30 14:32:51 +0100

    Assignment 03

--- user_input_interpolation/script.js
+++ user_input_interpolation/script.js
@@ -1,3 +1,7 @@
 // when the page is loaded the user will get a popup to write their Full Name and then another popup to write their Age.
 // Then, an alert will show up that reads something like this: Your name is {user-name} and you are {user-age} years old.
 
+let fullName = prompt('Please write your Full Name here: ');
+let age = parseInt(prompt('Please write your Age: '));
+
+console.log(`Your name is ${fullName} and you are ${age} years old`);
