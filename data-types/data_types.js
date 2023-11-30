c65119e82893092c4274139a219b662b5cbd5769
Legjende Shabani <legjendeshabani@gmail.com>
2023-11-30 14:19:00 +0100

    completed assignemnt 02

---
+++ data-types/data_types.js
@@ -0,0 +1,11 @@
+// Declare and initialize 5 variables using all the data types that we learned in this session.
+// These variables need to have meaningful names and should actually represent something - like using firstName for a String.
+
+const myName = 'Legjenda';
+const yearOfBirth = 1999;
+let isEmployeed = true;
+let age;
+
+// Log all of these variables in the console and look at the output.
+
+console.log(myName, yearOfBirth, isEmployeed, age);
