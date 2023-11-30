d6b73feefdf6eb2633d9d5ecd45104439baa2921
Legjende Shabani <legjendeshabani@gmail.com>
2023-11-30 14:25:43 +0100

    //

---
+++ user_input_interpolation/script.js
@@ -0,0 +1,3 @@
+// when the page is loaded the user will get a popup to write their Full Name and then another popup to write their Age.
+// Then, an alert will show up that reads something like this: Your name is {user-name} and you are {user-age} years old.
+
