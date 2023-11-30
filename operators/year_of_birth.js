63755e2f683780e8ab1f05d83c76d1f4287cae97
Legjende Shabani <legjendeshabani@gmail.com>
2023-11-30 15:27:43 +0100

    completed assignemnt 04

--- operators/year_of_birth.js
+++ operators/year_of_birth.js
@@ -2,6 +2,6 @@
 // You are {age} years old so you were probably born in the year {yearOfBirth}.
 // Think of a mathematical way to detemine the year of birth based on the age. Good luck!
 
-let age = parseInt(prompt('Your age is: '))
+let age = parseInt(prompt('Your age is: '));
 let yearOfBirth = 2023 - age;
 console.log(`You are ${age} years old so you were probably born in the year ${yearOfBirth}`);
