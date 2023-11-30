077b954bef443658bc7c54538356fc72cf6226bb
Legjende Shabani <legjendeshabani@gmail.com>
2023-11-30 14:01:38 +0100

    first commit

---
+++ variable_01.js
@@ -0,0 +1,15 @@
+// Create two variables called age and yearOfBirth and initialize them with your actual age and year of birth
+// Log both of these variables in the console and see their values
+
+let age = 24;
+let yearOfBirth = 1999;
+
+console.log(age, yearOfBirth);
+
+// After this change the values inside both of the variables
+// Finally log both of the variables in the console again
+
+age = 23;
+yearOfBirth = 2000;
+
+console.log(age, yearOfBirth);
