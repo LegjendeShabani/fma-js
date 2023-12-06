4fc4d571b844b55431dac4d057ccb9f86180d8f0
Legjende Shabani <legjendeshabani@gmail.com>
2023-12-06 16:30:29 +0100

    even or odd

---
+++ even-or-odd/script.js
@@ -0,0 +1,13 @@
+// Create a program that detects whether a number is even (0, 2, 4, 6...)
+//  or odd (1, 3, 5, 7, ...) and then output it in the console. 
+// The number should be taken from the user.
+
+let giveNumber = parseInt(prompt('Type a number: '));
+let even_or_odd;
+
+if (giveNumber % 2 == 0) {
+    console.log('The number you have typed is Even');
+}
+else {
+    console.log('The number you have typed is Odd');
+}
