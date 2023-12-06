bef55287adfb228aefe5c5c8be615b7e9bdf12da
Legjende Shabani <legjendeshabani@gmail.com>
2023-12-06 21:21:42 +0100

    boring calculator

---
+++ boring-calculator/script.js
@@ -0,0 +1,26 @@
+// Enhance the Boring Calculator example to also support the modulus operator.
+//  Furthermore, use the switch() statement to check for the operator instead of a lot of else-ifs.
+
+let a = parseFloat(prompt('Type a number: '))
+let b = parseFloat(prompt('Type another number: '))
+let result = prompt('What function do you want to use: ')
+
+switch (result) {
+    case 'modulo':
+        console.log(a % b);
+        break;
+    case 'division':
+        console.log(a / b);
+        break;
+    case 'multiplication':
+        console.log(a * b);
+        break;
+    case 'addition':
+        console.log(a + b);
+        break;
+    case 'subtraction':
+        console.log(a - b);
+        break;
+    default:
+        console.log('Never heard of it');
+}
