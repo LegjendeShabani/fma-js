38f271597c06ece5fac90e6f2febaf49792e42b2
Legjende Shabani <legjendeshabani@gmail.com>
2023-12-08 14:28:08 +0100

    progress bar

--- progress-bar/script.js
+++ progress-bar/script.js
@@ -1,26 +1,41 @@
-let percentage = parseInt(prompt('What is the progress?'))
+let percentage = parseInt(prompt('What is the progress?'));
+let color;
+
 
 if (isNaN(percentage) || percentage < 0 || percentage > 100) {
-  throw new Error('Write a valid percentage')
+    throw new Error('Write a valid percentage');
 }
 
-let color
 
-switch (true) {
-  case percentage <= 20:
-    color = 'black'
-    break
-  case percentage <= 50:
-    color = 'orange'
-    break
-  case percentage <= 80:
-    color = 'navy'
-    break
-  case percentage <= 100:
-    color = 'yellow'
-    break
-  default:
-    color = 'green'
+// switch (true) {
+//   case percentage <= 20:
+//     color = 'black'
+//     break
+//   case percentage <= 50:
+//     color = 'orange'
+//     break
+//   case percentage <= 80:
+//     color = 'navy'
+//     break
+//   case percentage <= 100:
+//     color = 'yellow'
+//     break
+//   default:
+//     color = 'green'
+// }
+
+if (percentage <= 20) {
+    color = 'black';
+} else if (percentage <= 50) {
+    color = 'orange';
+}
+else if (percentage <= 80) {
+    color = 'red';
+} else if (percentage <= 100) {
+    color = 'yellow';
+}
+else {
+    color = 'green';
 }
 
 let progressNumber = document.getElementById('progressNumber')
@@ -29,4 +44,4 @@ progressNumber.textContent = `${percentage}%`
 let progressBar = document.getElementById('bar')
 progressBar.style.backgroundColor = color
 progressBar.style.height = `${percentage}%`
-progressBar.setAttribute('title', percentage)
+progressBar.setAttribute('title', `${percentage}%`)
